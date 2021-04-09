44a54d2ed53504ab4620179948acc002b0e83def
matidav <matidav@sas.upenn.edu>
2021-04-08 20:13:03 -0400

    commit
    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-quotes */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import HomePage from './components/HomePage'
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/'>
+        <HomePage />
+      </Route>
+      <Route path='/signup'>
+        <SignUp />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
+    </Switch>
+  </Router>
+)
+
+export default App
